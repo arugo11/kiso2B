fec5d701f4c8dd196243327bf1e36f0c1da7f57a
arugo11 <al23088@shibaura-it.ac.jp>
2024-12-05 14:18:07 +0900

    fix: lesson5の演習を修正し、大文字変換のロジックを改善

--- lesson5/exercise.s
+++ lesson5/exercise.s
@@ -1,33 +1,27 @@
+
         .text
         .globl  main
 main:
-        la      $a0,    msg
-        li      $s1,    0x41    #0x41 is "A"
-        li      $s2,    0x5a    #0x5a is "Z"
-        li      $s3,    0x61    #0x61 is "a"
-        li      $s4,    0x71    #0x71 is "z"
+        la  $a0, msg
+        li  $s1, 0x41   #A in ascii
+        li  $s2, 0x5a   #Z in ascii
 
-loop:
-        add     $t2, $a0, $t1   #t2 : msgの先頭からn文字目
-        lb      $t0, 0($t2)
-        beq     $t0, $zero, exit#null文字なら終了
-        blt     $t0, $s1, next  #"A"より小さいときアルファベットでないためパス
-        ble     $t0, $s2, upper #大文字のため大文字変換へ
-        blt     $t0, $s3, next  #アルファベットでない
-        bgt     $t0, $s4, next  #アルファベットでない
-upper:
-        xor      $t0, $t0, 0x20 #大文字と小文字の変換
+loop_start:
+    add     $t2, $a0, $t1   # t2 : 頭から何文字目か
+    lb      $t0, 0($t2)
+    beq     $t0, 0x00, exit # t2 == null => exit
+    blt     $t0, 0x41, next # t2 < A => next : not large
+    bgt     $t0, 0x5A, next # t2 > Z => next : not large
+    addi    $s0, $s0, 1     # 大文字と判定
+
 next:
-        sb      $t0, 0($t2)     #msgに上書き
-        addi    $t1, $t1, 1     #インクリメント
-        j       loop
-exit:
-        li      $v0, 4
-        la      $a0, msg
-        syscall
+    addi    $t1, $t1, 1
+    j       loop_start
 
-        li      $v0, 10
-        syscall
-        jr      $ra
-        .data
-msg:    .ascii "OmIyA8\n"
+exit:
+    li      $v0, 10
+    syscall
+    jr      $ra
+
+.data
+msg:        .ascii  "Hello, World!\n"
